82574bd0272ad73dc6412fedfbb8cd7df29ed9c7
vinavinavina <vinicius.antoniacomi.reis@gmail.com>
2025-06-10 12:34:02 -0300

    Update listaexercicio3_resolvido (1).py

--- listaexercicio3_resolvido (1).py
+++ listaexercicio3_resolvido (1).py
@@ -91,6 +91,7 @@ print(f"Projeto5: R$ {valor_presente(df['Projeto5'], 0.02):,.2f}")
 
 """5) Gere um gráfico de dispersão cruzando os dados do `Projeto1` e `Projeto2`, com marcadores verdes e em formato de estrela. (Peso: 2,0)"""
 
+st.code(code, language ='python')
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
